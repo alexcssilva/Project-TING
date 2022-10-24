9f7705561548a74dd326d4d380d4890bae61ada0
Alex Silva <alexcssilva@gmail.com>
2022-10-24 10:15:27 -0300

    feat: added file_management.py - Desafio 02 concluido

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+from os.path import exists
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
+
+    if '.txt' not in path_file:
+        print("Formato inválido", file=sys.stderr)
+        return None
+
+    with open(path_file, "r") as path:
+        return path.read().split("\n")
