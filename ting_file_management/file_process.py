827f475e19f2effbb1d29d0dcd598a29701034a2
Alex Silva <alexcssilva@gmail.com>
2022-10-24 11:02:38 -0300

    feat: added file_process.py - Desafio 03 concluido

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for row in range(len(instance)):
+        if instance.search(row)["nome_do_arquivo"] == path_file:
+            return None
+
+    file = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    instance.enqueue(result)
+
+    sys.stdout.write(str(result))
 
 
 def remove(instance):
